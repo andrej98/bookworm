b915727f2b03bb92b14380fcb8622f9c4edcb1cc
Andrej Valíček <andrej.valicek98@gmail.com>
2021-11-21 18:52:09 +0100

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+const App = () => (
+	// <UserProvider>
+	// 	<LanguageProvider>
+	// 		<ThemeProvider theme={theme}>
+	// 			<BrowserRouter>
+	// 				<CssBaseline />
+	// 				<Layout>
+	// 					<Routes />
+	// 				</Layout>
+	// 			</BrowserRouter>
+	// 		</ThemeProvider>
+	// 	</LanguageProvider>
+	// </UserProvider>
+	<div>
+		<h1>BookWorm</h1>
+	</div>
+);
+
+export default App;
