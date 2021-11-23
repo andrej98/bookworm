aaeda8779a05c03a762d9dad5341ebaa3819d5c0
Miriam <miriam.cabadajova1@gmail.com>
2021-11-23 21:26:15 +0100

    create ChooseImage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import ChooseImage from './components/ChooseImage';
 import { UserProvider } from './hooks/useLoggedInUser';
 
 const App = () => (
 	<UserProvider>
 		<div>
 			<h1>BookWorm</h1>
+			<ChooseImage />
 		</div>
 	</UserProvider>
 );
