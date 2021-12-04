1c2c2940c4deb914bb717e41bcb9c06df0b2766a
Miriam <miriam.cabadajova1@gmail.com>
2021-12-04 09:05:39 +0100

    move table pagination to separate component

--- src/components/BooksTable.tsx
+++ src/components/BooksTable.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from 'react';
-import { useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -10,10 +8,6 @@ import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import IconButton from '@mui/material/IconButton';
-import FirstPageIcon from '@mui/icons-material/FirstPage';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import LastPageIcon from '@mui/icons-material/LastPage';
 import { onSnapshot, deleteDoc } from 'firebase/firestore';
 import { TableHead } from '@mui/material';
 import { Delete, Edit } from '@mui/icons-material';
@@ -24,6 +18,7 @@ import { useLoggedInUser } from '../hooks/useLoggedInUser';
 import BookDialog from './BookDialog';
 import ConfirmDialog from './ConfirmDialog';
 import Filter from './Filter';
+import TablePaginationActions from './TablePagination';
 
 type Column = {
 	id: 'booktitle' | 'author' | 'category';
@@ -42,86 +37,6 @@ const columns: readonly Column[] = [
 		align: 'right'
 	}
 ];
-
-type TablePaginationActionsProps = {
-	count: number;
-	page: number;
-	rowsPerPage: number;
-	onPageChange: (
-		event: React.MouseEvent<HTMLButtonElement>,
-		newPage: number
-	) => void;
-};
-
-const TablePaginationActions = (props: TablePaginationActionsProps) => {
-	const theme = useTheme();
-	const { count, page, rowsPerPage, onPageChange } = props;
-
-	const handleFirstPageButtonClick = (
-		event: React.MouseEvent<HTMLButtonElement>
-	) => {
-		onPageChange(event, 0);
-	};
-
-	const handleBackButtonClick = (
-		event: React.MouseEvent<HTMLButtonElement>
-	) => {
-		onPageChange(event, page - 1);
-	};
-
-	const handleNextButtonClick = (
-		event: React.MouseEvent<HTMLButtonElement>
-	) => {
-		onPageChange(event, page + 1);
-	};
-
-	const handleLastPageButtonClick = (
-		event: React.MouseEvent<HTMLButtonElement>
-	) => {
-		onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-	};
-
-	return (
-		<Box sx={{ flexShrink: 0, ml: 2.5 }}>
-			<IconButton
-				onClick={handleFirstPageButtonClick}
-				disabled={page === 0}
-				aria-label="first page"
-			>
-				{theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
-			</IconButton>
-			<IconButton
-				onClick={handleBackButtonClick}
-				disabled={page === 0}
-				aria-label="previous page"
-			>
-				{theme.direction === 'rtl' ? (
-					<KeyboardArrowRight />
-				) : (
-					<KeyboardArrowLeft />
-				)}
-			</IconButton>
-			<IconButton
-				onClick={handleNextButtonClick}
-				disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-				aria-label="next page"
-			>
-				{theme.direction === 'rtl' ? (
-					<KeyboardArrowLeft />
-				) : (
-					<KeyboardArrowRight />
-				)}
-			</IconButton>
-			<IconButton
-				onClick={handleLastPageButtonClick}
-				disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-				aria-label="last page"
-			>
-				{theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
-			</IconButton>
-		</Box>
-	);
-};
 
 type Props = {
 	isRead?: boolean;
@@ -152,7 +67,6 @@ const BooksTable = ({ isRead }: Props) => {
 		};
 	}, []);
 
-	// Avoid a layout jump when reaching the last page with empty rows.
 	const emptyRows =
 		page > 0 ? Math.max(0, (1 + page) * rowsPerPage - filteredBooks.length) : 0;
 
