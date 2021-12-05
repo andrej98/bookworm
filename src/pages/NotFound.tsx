acc53372acb94aabdca6da2251e8b654a453d166
Andrej Valíček <andrej.valicek98@gmail.com>
2021-12-05 23:20:04 +0100

    not found layout

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,12 +1,20 @@
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import WarningIcon from '@mui/icons-material/Warning';
 
 const NotFound = () => (
-	<>
+	<Box
+		sx={{
+			display: 'flex',
+			flexDirection: 'column',
+			justifyContent: 'center',
+			alignItems: 'center',
+			mt: '100px'
+		}}
+	>
 		<WarningIcon sx={{ typography: 'h1' }} />
 		<Typography variant="h2">Not found</Typography>
 		<Typography>This page does not exist :(</Typography>
-	</>
+	</Box>
 );
 
 export default NotFound;
